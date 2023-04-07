e83d6fd04a6ada29fead33b3e2cf379c4978c18d
cocojambo02 <cosmin.neamtiu@stud.ubbcluj.ro>
2023-04-07 23:04:10 +0300

    Resolved user commit on article, memory error on photo submit, rethinking the whole article commit

--- app/src/main/java/Utils/FirestoreUtils.java
+++ app/src/main/java/Utils/FirestoreUtils.java
@@ -26,11 +26,9 @@ public class FirestoreUtils {
     }
 
     public void commitArticle(Context context){
-        Article temp = customTestObject();
         db.collection("Users" ).document(article.getCreator()).collection("Articles").document(article.getArticleId())
                 .set(article)
                 .addOnSuccessListener(aVoid -> Toast.makeText(context,"Article submitted successfully!",Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Toast.makeText(context,"Error submitting article!",Toast.LENGTH_SHORT).show());
-
     }
 
     public void updateArticle(){
@@ -41,27 +39,5 @@ public class FirestoreUtils {
 
     }
 
-    private Map<Object, Object> getArticleAsMap(){
-        Map<Object, Object> articleAsMap = new HashMap<>();
-        articleAsMap.put("name", "Los Angeles");
-        articleAsMap.put("state", "CA");
-        articleAsMap.put("country", "USA");
-        return articleAsMap;
-    }
 
-    private Article customTestObject(){
-        Title title = new Title();
-        Article article1 = new Article(article.getCreator());
-
-        //Paragraph p1 = new Paragraph("1", title,"", "");
-        //Paragraph p2 = new Paragraph("2", title,"", "");
-        //article1.addNewParagraph(p1);
-        //article1.addNewParagraph(p2);
-
-        Image i1 = new Image(article.getArticleId(), "", "", "", false, new ArrayList<>());
-        Image i2 = new Image(article.getArticleId() + "plm", "", "", "", false, new ArrayList<>());
-        article1.addNewImage(i1);
-        article1.addNewImage(i2);
-        return article1;
-    }
 }
